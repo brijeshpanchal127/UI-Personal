a1d18bca241053308bc9536ecb0afcc3fa970de9
mahivkpatel <mahivkpatel@gmail.com>
2021-08-24 13:32:13 +0530

    external js

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,7 +1,4 @@
-// function myFunction(p1, p2) {
-//     return p1 * p2;}
-//     console.log(myFunction(3,4))
-
-function pass_function(){
-    alert('hello');
-  }
+exports.helloWorld = function () {
+  alert("test");
+  console.log("Hello World!");
+};
