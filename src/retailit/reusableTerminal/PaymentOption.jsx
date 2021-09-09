9a7db767348d8253886501290f0d2181a490b15a
mahivkpatel <mahivkpatel@gmail.com>
2021-09-09 19:40:49 +0530

    added styles js

--- src/retailit/reusableTerminal/PaymentOption.jsx
+++ src/retailit/reusableTerminal/PaymentOption.jsx
@@ -1,61 +1,15 @@
 import React, { useState, useEffect } from "react";
-import "./PaymentOption.css";
+import "./PaymentOption.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSelector } from "react-redux";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
-import CreditCardIcon from "@material-ui/icons/CreditCard";
-import ReceiptIcon from "@material-ui/icons/Receipt";
-import AppleIcon from "@material-ui/icons/Apple";
-import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
-import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
-import * as Icons from "@material-ui/icons/";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import Divider from "@material-ui/core/Divider";
-
-const useStyles = makeStyles((theme) => ({
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-  // submit_btn: {
-  //   backgroundColor: "#72BB53",
-  //   color: "#ffffff",
-  // },
-  option_list: {
-
-    padding: "5%",
-  },
-  item: {
-    "& #amount_input": {
-      margin: "0 0 0 4px",
-      width: "25%",
-      padding: "2%",
-    },
-    "& #button_submit": {
-      backgroundColor: "#72BB53",
-      width: "100%",
-      color: "#ffffff",
-      padding: "4%",
-      border: "none",
-      borderRadious:"20px"
-    },
-    "& .radio_btn": { padding: "20%", margin: "4%" },
-    "& .amount_div": { display: "flex", margin: "2%" },
-    "& .teminal_label": { padding: "1px 10px 1px 3px", fontSize: "x-small" },
-    "& .amount_label": {
-      fontSize: "x-small",
-      paddingTop: "6px",
-      marginRight: "9px",
-    },
-
-  },
-}));
+import { useStyles } from "./styles.js";
 
 const PaymentOption = (props) => {
   const classes = useStyles();
@@ -107,13 +61,11 @@ const PaymentOption = (props) => {
                 unmountOnExit
               >
                 <div className={classes.option_list}>
-
                   <div
                     id={option.div_id}
                     key={option.key}
                     className={classes.item}
                   ></div>
-
                 </div>
               </Collapse>
               <Divider />
