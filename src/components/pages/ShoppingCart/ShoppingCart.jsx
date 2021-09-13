ede7053ff8631ced3376d81d14ce5dfe81cf6ab8
mahivkpatel <mahivkpatel@gmail.com>
2021-09-13 11:53:58 +0530

    css changes for shoppingCardMainPanel

--- src/components/pages/ShoppingCart/ShoppingCart.jsx
+++ src/components/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,27 +1,13 @@
 import React, { useEffect } from "react";
-// import MainPanel from "./MainPanel/MainPanel.jsx";
 import SideBar from '../Landing/SideBar/SideBar.jsx';
 import ShoppingCartMainPanel from './ShoppingCartMainPanel/ShoppingCartMainPanel.jsx';
-import {
-  cacheStoresData
-} from "../../../reducers/actions/landing.action";
-import landingService from "../../../services/landing.service";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from 'react-router-dom';
-import { selectCurrentFunction } from "../../../reducers/actions/landing.action";
-import { selectDisplayStoreSelection, selectDisplayStorePos } from "../../../reducers/actions/display.action";
 
 const ShoppingCart = () => {
-
-  const dispatch = useDispatch();
-  const auth = useSelector(state => state.login.auth);
-  const selectedStore = useSelector(state => state.landing.selectedStore);
-  const currentStoreFunction = useSelector(state => state.landing.currentStoreFunction);
-
   const isLoggedIn = useSelector(state => state.login.isLoggedIn);
 
   if (!isLoggedIn) {
-    console.log("/shopping: redirecting to /");
     return <Redirect to="/" />;
   }
 
--- src/components/pages/ShoppingCart/ShoppingCartMainPanel/ShoppingCartMainPanel.jsx
+++ src/components/pages/ShoppingCart/ShoppingCartMainPanel/ShoppingCartMainPanel.jsx
@@ -1,171 +1,65 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import data from '../../../../data/data';
-
-import Detail from '../Detail/Detail.jsx';
-import Inventory from '../Inventory/Inventory.jsx';
-
-import "./ShoppingCartMainPanel.css";
-import "../Inventory/Inventory.css";
-
-import { fade, makeStyles } from '@material-ui/core/styles';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-    marginTop: '20px',
-  },
-  inventoryParent: {
-    display: 'flex',
-    flexDirection: 'column',
-    fontSize: '13px',
-    marginTop: '0.5rem',
-    label: {
-      color: 'grey'
-    }
-  },
-  inventoryFirstRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '90%',
-    marginRight: '1rem',
-  },
-  inventorySecondRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '90%',
-    marginTop: '0.5rem',
-    marginRight: '1rem',
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: "white",
-    marginTop: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  iconContainer: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    position: 'relative',
-    bottom: 20,
-    '&:hover': {
-      cursor: "pointer",
-    },
-  },
-  addButton: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    position: 'relative',
-    bottom: 20,
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
+import React from "react";
+import data from "../../../../data/data";
+import Detail from "../Detail/Detail.jsx";
 
 export default function MainPanel() {
-  const classes = useStyles();
-
-
-  const storeSelected = useSelector(state => state.landing.storeSelected);
-  const storeLocations = useSelector(state => state.landing.storeLocations);
-  const currentStoreFunction = useSelector(state => state.landing.currentStoreFunction);
-
-  const currentDisplayProfile = useSelector(state => state.display.displayProfile);
-  var currentDisplay = data.DISPLAY_PROFILE.find(o => o.name === currentDisplayProfile);
-
   return (
-    // <div className="shoppingCart__main">
-    //  <div className="shoppingCart__main--inventory">
-    //    <Inventory /></div> 
-
-    //   <div className="shoppingCart__main--detail">
-    //     <Detail />
-    //   </div>
-    // </div>
-
     <div className="shoppingCart__main">
       <div className="shoppingCart__main--inventory">
         <div className="inventory">
-
           {/* Search bar */}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+          <div className={"search"}>
+            <div className={"searchIcon"}>
+              <i class="fa fa-search"></i>
             </div>
-            <InputBase
-              // placeholder={storeSelected === false ? "Store" : "Store / Message" }
-              placeholder={currentDisplay.searchInputPlaceholder}
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
+            <div className="MuiInputBase-root inputRoot">
+              <input
+                placeholder="Store"
+                type="text"
+                aria-label="search"
+                class="MuiInputBase-input inputInput"
+                value=""
+              />
+            </div>
           </div>
 
           {/* Invetory panel */}
-          <List component="nav" aria-label="main mailbox folders" className={classes.root}>
-            {data.inventory.map((item, index) => (
-              <>
-                {index === 0 ? <Divider /> : null}
-                <ListItem className={classes.inventoryParent} key={item.NAME}>
-                  <div className={classes.inventoryFirstRow}>
-                    <div ><span className="inventory__card--label">SKU: </span>{item.SKU}</div>
-                    <div ><span className="inventory__card--label">UID: </span>{item.UID}</div>
-                    <div ><span className="inventory__card--label">QTY: </span>{item.QTY}</div>
-                    <div ><span className="inventory__card--label">PRICE: </span>{item.PRICE}</div>
-                  </div>
-                  <div className={classes.inventorySecondRow}>
-                    <div>{item.NAME}</div>
-                  </div>
-                  <div className={classes.iconContainer}>
-                    <MoreVertIcon />
-                  </div>
-                </ListItem>
-                <Divider />
-              </>
-            ))}
-          </List>
+          <div className="shoppingCart__main_root">
+            <ul component="nav" aria-label="main mailbox folders">
+              {data.inventory.map((item, index) => (
+                <>
+                  {index === 0 ? <hr class="solid" /> : null}
+                  <li id="shoppingInventoryParent" key={item.NAME}>
+                    <div className="shoppingInventoryFirstRow">
+                      <div>
+                        <span className="inventory__card--label">SKU: </span>
+                        {item.SKU}
+                      </div>
+                      <div>
+                        <span className="inventory__card--label">UID: </span>
+                        {item.UID}
+                      </div>
+                      <div>
+                        <span className="inventory__card--label">QTY: </span>
+                        {item.QTY}
+                      </div>
+                      <div>
+                        <span className="inventory__card--label">PRICE: </span>
+                        {item.PRICE}
+                      </div>
+                    </div>
+                    <div className={"shopping_InventorySecondRow"}>
+                      <div>{item.NAME}</div>
+                    </div>
+                    <div className={"iconContainer"}>
+                      <hr class="solid" />
+                    </div>
+                  </li>
+                  <hr class="solid" />
+                </>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -173,7 +67,6 @@ export default function MainPanel() {
       <div className="shoppingCart__main--detail">
         <Detail />
       </div>
-
     </div>
   );
 }
